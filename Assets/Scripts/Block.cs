87d8dc6e8bb1530869ce265029cb2e8a99914e4e
S.Vasilev <s.vasilev9169@gmail.com>
2019-06-18 23:49:26 +0300

    commit spowning of blocks and rows

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Block : MonoBehaviour
+{
+    private int remainingHits = 5;
+
+    private SpriteRenderer spriteRenderer;
+    private TMPro.TextMeshPro text;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        text = GetComponentInChildren<TextMeshPro>();
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        text.SetText(remainingHits.ToString());
+        spriteRenderer.color = Color.Lerp(Color.white, Color.red, remainingHits / 10f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        remainingHits--;
+        if (remainingHits > 0)
+        {
+            UpdateUI();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    internal void SetHits(int hits)
+    {
+        remainingHits = hits;
+        UpdateUI();
+    }
+}
---
+++ Assets/Scripts/BlockSpowner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpowner : MonoBehaviour
+{
+    private int playWidth = 8;
+    private float distanceBetweenBlocks = 1f;
+    private int rowsSpowned;
+
+    [SerializeField]
+    private Block blockPref;
+
+    private List<Block> blocksSpowned = new List<Block>();
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            SpownRowOfBlocks();
+        }
+    }
+
+    private void SpownRowOfBlocks()
+    {
+        foreach (var block in blocksSpowned)
+        {
+            if (block != null)
+            {
+                block.transform.position += Vector3.down * distanceBetweenBlocks;
+            }
+        }
+
+
+        for (int i = 0; i < playWidth; i++)
+        {
+            //around 30% chance to get random <= 30
+            if (UnityEngine.Random.Range(0, 100) <= 30)
+            {
+                var block = Instantiate(blockPref, GetPosition(i), Quaternion.identity);
+                int hits = UnityEngine.Random.Range(1, 3) + rowsSpowned;
+
+                block.SetHits(hits);
+
+                blocksSpowned.Add(block);
+            }
+        }
+        rowsSpowned++;
+    }
+
+    private Vector3 GetPosition(int i)
+    {
+        Vector3 pos = transform.position;
+        pos += Vector3.right * i * distanceBetweenBlocks;
+        return pos;
+    }
+}
